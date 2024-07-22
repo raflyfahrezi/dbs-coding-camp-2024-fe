f6933c1e294af8c34cd61b996c33af61323d942a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-07-22 14:11:25 +0700

    add demo -2

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 class Heading extends HTMLElement {
+  static observedAttributes = ['color']
+
   constructor() {
     super()
 
+    this._text = this.getAttribute('text')
+    this._color = this.getAttribute('color')
+  }
+
+  connectedCallback() {
     this.render()
+
+    console.log('Component Connected')
+  }
+
+  disconnectedCallback() {
+    console.log('Component Disconnected')
   }
 
   render() {
-    this.innerHTML = `<h1>Hello World</h1>`
+    this.innerHTML = `<h1 style="color: ${this._color};">${this._text}</h1>`
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Atribut ${name} diubah.`)
+    console.log(`Nilai lama adalah ${oldValue}`)
+    console.log(`Nilai baru adalah ${newValue}`)
+
+    // Ubah nilai atribut yang telah disimpan
+    this[`_${name}`] = newValue
+
+    // Render konten ulang
+    this.render()
   }
 }
 
 customElements.define('my-heading', Heading)
 
-// buatlah versi tombol
+// initialize
 
-class Button extends HTMLElement {}
+const myHeading = document.getElementById('myHeading')
+const buttonChange = document.getElementById('buttonChange')
+const buttonDelete = document.getElementById('buttonDelete')
 
-// define custom element
+// update attribute
+
+buttonChange.addEventListener('click', () => {
+  myHeading.setAttribute('color', 'blue')
+})
+
+// delete component
+
+buttonDelete.addEventListener('click', () => {
+  myHeading.remove()
+})
