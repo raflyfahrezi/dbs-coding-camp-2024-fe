0c2a9ef1c2d9e779d05e1f97c43095692ce4de0c
Hasan Ismail <hasanqqsp@gmail.com>
2024-08-27 14:53:32 +0700

    8-starter

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -1,71 +1,48 @@
-import Swal from "sweetalert2";
-
 const BASE_URL = "https://books-api.dicoding.dev";
 //  TODO : Buat fungsi untuk memanggil API disini
-function showAlert(error) {
-  Swal.fire({
-    title: "Oops...",
-    text: error.message,
-    icon: "error",
-    confirmButtonColor: "#3085d6",
-    confirmButtonText: "OK",
-  });
-}
 function getAllBooks() {
-  return fetch(`${BASE_URL}/list`)
-    .then((response) => response.json())
-    .then((data) => data.books)
-    .catch((err) => showAlert(err));
+    return fetch(`${BASE_URL}/list`)
+        .then((response) => response.json())
+        .then((data) => data.books);
 }
 
 async function addBook({ id, title, author }) {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Auth-Token": "12345",
-    },
-    body: JSON.stringify({ id, title, author }),
-  };
-  try {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Token": "12345",
+        },
+        body: JSON.stringify({ id, title, author }),
+    };
     const response = await fetch(`${BASE_URL}/add`, options);
     const responseJSON = await response.json();
     return responseJSON;
-  } catch (err) {
-    showAlert(err);
-    throw err;
-  }
 }
 
 async function editBook({ id, title, author }) {
-  const options = {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Auth-Token": "12345",
-    },
-    body: JSON.stringify({ id, title, author }),
-  };
-  try {
+    const options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Token": "12345",
+        },
+        body: JSON.stringify({ id, title, author }),
+    };
     const response = await fetch(`${BASE_URL}/edit/${id}`, options);
     const responseJSON = await response.json();
     return responseJSON;
-  } catch (err) {
-    showAlert(err);
-    throw err;
-  }
 }
 
 async function deleteBook(id) {
-  return fetch(`${BASE_URL}/delete/${id}`, {
-    method: "DELETE",
-    headers: {
-      "X-Auth-Token": "12345",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => data.books)
-    .catch((err) => showAlert(err));
+    return fetch(`${BASE_URL}/delete/${id}`, {
+        method: "DELETE",
+        headers: {
+            "X-Auth-Token": "12345",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => data.books);
 }
 
 export { getAllBooks, addBook, editBook, deleteBook };
